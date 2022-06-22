87fdf055d906d41c09c2de6b7f5a89885445492c
Sergio <winksousa0@gmail.com>
2022-06-22 20:38:27 -0300

    aula 03

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./components/Router";
+import { client } from "./lib/apollo";
 import { Event } from "./pages/Event";
 
 function App() {
   return (
-    <>
-      <Event />
-    </>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
